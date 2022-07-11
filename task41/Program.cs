583face3e4929fa801d72a807d1354e9be27f09a
DmitryZ <zagarskih@inbox.ru>
2022-07-11 19:34:16 +0500

    готово

---
+++ task41/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+System.Console.WriteLine("Введите количество чисел");
+int n = int.Parse(Console.ReadLine()!);
+int[] array = new int[n];
+for (int i = 1; i < n + 1; i++)
+{
+    System.Console.WriteLine($"Введите число № {i}: ");
+    array[i - 1] = int.Parse(Console.ReadLine()!);
+}
+int summ = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > 0)
+    {
+        summ++;
+    }
+}
+System.Console.WriteLine($"Количество положительных чисел равно: {summ}");
---
+++ task43/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями 
+// y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+System.Console.WriteLine("Введите значения k1 и b1 в уравнении y = k1 * x + b1");
+System.Console.Write("b1 = ");
+double b1 = double.Parse(Console.ReadLine()!);
+System.Console.Write("k1 = ");
+double k1 = double.Parse(Console.ReadLine()!);
+System.Console.WriteLine("Введите значения k2 и b2 в уравнении y = k2 * x + b2");
+System.Console.Write("b2 = ");
+double b2 = double.Parse(Console.ReadLine()!);
+System.Console.Write("k2 = ");
+double k2 = double.Parse(Console.ReadLine()!);
+if (k1 == k2 && b1 != b2) { System.Console.WriteLine("Прямые не пересекаются - параллельны"); }
+else
+{
+    if (k1 == k2 && b1 == b2) { System.Console.WriteLine("Прямые совпадают"); }
+    else
+    {
+        double x = (b2 - b1) / (k1 - k2);
+        double y = k1 * x + b1;
+        x = Math.Round(x, 2);
+        y = Math.Round(y, 2);
+        System.Console.WriteLine($"Координаты точки пересечения прямых ({x} ; {y})");
+    }
+}
